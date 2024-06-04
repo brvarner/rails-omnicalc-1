a7ff8a60478a967843f9b52c3f6ffcbc64697f8b
Brandon Varner <84243251+brvarner@users.noreply.github.com>
2024-06-04 16:36:00 +0000

    added routing elements and calculations

---
+++ app/controllers/calc_controller.rb
@@ -0,0 +1,58 @@
+class CalcController < ApplicationController
+  def square
+    render({:template => "res_templates/square"})
+  end
+
+  def square_res
+    @target_num = params.fetch("num").to_i
+
+    @res = @target_num * @target_num
+
+    render({:template => "res_templates/square_res"})
+  end
+
+  def square_root
+    render({:template => "res_templates/square_root"})
+  end
+
+  def square_root_res
+    @target_num = params.fetch("num").to_i
+    
+    @res = Math.sqrt(@target_num)
+
+    render({:template => "res_templates/square_root_res"})
+  end
+
+  def payment
+    render({:template => "res_templates/payment"})
+  end
+  
+  def payment_res
+    @years_left = params.fetch('years_remaining').to_i
+    @apr = params.fetch('apr').to_f
+    @pv = params.fetch('present_value').to_f
+  
+    r = (@apr / 100) / 12
+    n = @years_left * 12
+  
+    numerator = (r * @pv)
+    denominator = 1 - ((1 + r) ** -n)
+  
+    @payment =  numerator / denominator
+  
+    render({:template => "res_templates/payment_res"})
+  end
+
+  
+  def random
+    render({:template => "res_templates/random"})
+  end
+
+  def random_res
+    @min = params.fetch('min').to_f 
+    @max = params.fetch('max').to_f 
+    @rand = rand(@min...@max) 
+
+    render({:template => "res_templates/random_res"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  
+  get("/", :controller => "calc", :action => "square")
+  get("/square/new", :controller => "calc", :action => "square")
+  get("/square/results", :controller => "calc", :action => "square_res")
+  get("/square_root/new", :controller => "calc", :action => "square_root")
+  get("/square_root/results", :controller => "calc", :action => "square_root_res")
+  get("/payment/new", :controller => "calc", :action => "payment")
+  get("/payment/results", :controller => "calc", :action => "payment_res")
+  get("/random/new", :controller => "calc", :action => "random")
+  get("/random/results", :controller => "calc", :action => "random_res")
 end
